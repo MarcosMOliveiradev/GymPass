56661c7f22b3e0d6373f222cb650576bb9a428f2
Marcos Monteiro <marcos.moliveira@outlook.com>
2023-12-26 10:34:41 -0300

    rota de perfil

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,12 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './controllers/registerController'
 import { authenticate } from './controllers/authenticate'
+import { profile } from './controllers/profile'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
+
   app.post('/sessions', authenticate)
+
+  app.get('/me', profile)
 }
